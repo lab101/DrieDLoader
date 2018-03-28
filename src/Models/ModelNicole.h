629e3aa34cdd3cfcc2b51450f94e5a26e655e003
Nicole Horward <nicole.horward@student.ehb.be>
2018-03-28 11:11:26 +0200

    Model Nicole

---
+++ src/Models/ModelNicole.h
@@ -0,0 +1,31 @@
+#pragma once
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+#include "IModel.h"
+
+class ModelNicole : public IModel {
+
+	ofxAssimpModelLoader modelLoader;
+	ofMesh faceMesh;
+
+	// original loaded mesh in a vbo
+	ofVboMesh vboMesh;
+
+	ofCamera camera;
+	ofLight light1;
+
+	ofFbo fbo;
+
+
+public:
+	void setup(string Path, float fboWidth, float fboHeight);
+	void update(float speed);
+	void draw();
+	void drawInFbo();
+	void drawFbo(float x, float y);
+
+
+
+};
