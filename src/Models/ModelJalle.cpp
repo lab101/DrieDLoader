468b6290e083261ce372830aefa0388561724562
Jalle Van Goidsenhoven <jalle.van.goidsenhoven@student.ehb.be>
2018-03-28 11:37:35 +0200

    jalle model animation changed

--- src/Models/ModelJalle.cpp
+++ src/Models/ModelJalle.cpp
@@ -36,6 +36,8 @@ void ModelJalle::setup(string path, float fboWidth, float fboHeight) {
 	light1.setPosition(0, 5, 5);
     
     fbo.allocate(fboWidth, fboHeight);
+
+
     
 }
 
@@ -44,16 +46,6 @@ void ModelJalle::update(float speed) {
 
 
     faceMesh.clearColors();
-    /*
-    // adjust the colors
-    float hueStart = fmod(ofGetElapsedTimef() * 10, 255);
-    for(ofVec3f& v : vboMesh.getVertices()){
-        float h = ofMap(v.x, -0.1, 0.1, hueStart, hueStart +10, true);
-        float s = ofMap(v.y, -0.1, 0.1, 255, 100, true);
-
-        faceMesh.addColor(ofColor::fromHsb(h, s, 255));
-    }
-    */
     // clearint the vertices and adding new ones with some noise
 	/*
 
@@ -94,8 +86,10 @@ void ModelJalle::draw() {
 	ofPushMatrix();
 
 
-        //(ofGetElapsedTimef() * 10);
-		ofRotateY(90);
+        ofRotateY((ofGetElapsedTimef() * 10));
+		//ofRotateY(90);
+		//ofRotateY(sin(ofGetElapsedTimef()));
+		ofRotateZ(sin(ofGetElapsedTimef() * 10) * 8);
         ofRotateX(-90);
         ofScale(1.5, 1.5, 1.5);
 
@@ -106,7 +100,7 @@ void ModelJalle::draw() {
 		for (ofVec3f& v : vboMesh.getVertices()) {
 			//ofDrawBox(v * 12, ofNoise(ofGetElapsedTimef() * 0.8, v.z) * 0.5);
 			//ofDrawSphere(v * 12, ofNoise(ofGetElapsedTimef() * 0.8, v.z) * 0.5);
-			ofDrawCylinder(v * 12, ofNoise(ofGetElapsedTimef() * 0.8, v.z) * 0.5, ofNoise(ofGetElapsedTimef() * 0.3, v.y) * 0.8);
+			ofDrawCylinder(v * 12, ofNoise(ofGetElapsedTimef() * 0.4, v.z) * 0.5, ofNoise(ofGetElapsedTimef() * 0.3, v.y) * 0.8);
 
 		}
         //faceMesh.drawFaces();
